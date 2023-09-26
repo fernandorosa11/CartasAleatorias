2b3970042bd0e90017308b6ed38c5fae1a76ad8b
ferna <ferna@LAPTOP-TC9GEVND>
2023-09-26 17:04:26 -0300

    Adicionar arquivos de projeto.

---
+++ CartasAleatorias/EscolherCarta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class CartaAleatoria
+{
+    static Random random = new Random();
+
+    public static string[] EscolherCarta(int numeroDeCartas)
+    {
+        string[] cartasEscolhidas = new string[numeroDeCartas];
+        for (int i = 0; i < numeroDeCartas; i++)
+        {
+            cartasEscolhidas[i] = ValorAleatorio() + " de " + NaipeAleatorio();
+        }
+        return cartasEscolhidas;
+    }
+
+    private static string ValorAleatorio()
+    {
+        int valor = random.Next(1, 14); // Retorna de 1 a 13
+        if (valor == 1) return "Ás";
+        if (valor == 11) return "Valete";
+        if (valor == 12) return "Dama";
+        if (valor == 13) return "Rei";
+        return valor.ToString();
+    }
+
+    private static string NaipeAleatorio()
+    {
+        int valor = random.Next(1, 5);
+        if (valor == 1) return "Espadas";
+        if (valor == 2) return "Copas";
+        if (valor == 3) return "Ouros";
+        return "Paus";
+    }
+}
---
+++ CartasAleatorias/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Entre a quantidade de cartas para serem escolhidas: ");
+        string linha = Console.ReadLine();
+        if (int.TryParse(linha, out int numeroDeCartas))
+        {
+            foreach (string carta in CartaAleatoria.EscolherCarta(numeroDeCartas))
+            {
+                Console.WriteLine(carta);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Por favor, entre um valor inteiro válido.");
+        }
+        Console.ReadLine();
+    }
+}
+
